7cae96c3d92ad56668cd30837df44c981df1d319
cmgjsw <cmgjsw@163.com>
2020-07-29 02:58:03 +0800

    20200729

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
 import { ToyReact,Component } from './ToyReact.js'
 
-class MyComponent extends Component{
+class Square extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+   
     render() {
-        return <div>
-            <span>hello! ToyReact.</span>
+        return (
+            <button className="square" onClick={() => {              
+                if (this.state.value === null || this.state.value === 'O') {
+                    this.setState({ value: 'X'})
+                } else {
+                    this.setState({ value: 'O'})
+                } 
+              }
+            }>
+                {this.state.value ? this.state.value: ""}
+            </button> 
+          
+        );
+    }
+
+}
+class Board extends Component{
+    renderSquare(i) {
+        return (
+            <Square
+                value={i}
+            />
+        );
+    }
+
+    render() {
+        return (
             <div>
-                {this.children}
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-        </div>
+        );
     }
 
 }
 
-let a = <MyComponent name="a" id="ida"><div>dddd</div></MyComponent>
+let a = <Board />
 
 ToyReact.render(
     a,
